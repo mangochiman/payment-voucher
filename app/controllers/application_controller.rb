68885c98c19908843a7c0ce333d1c1b389f9fc3e
mangochiman <mangochiman@gmail.com>
2017-09-25 06:43:49 +0200

    Error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,5 +33,12 @@ class ApplicationController < ActionController::Base
       return true
     end
   end
-  
+
+  def rescue_action(exception)
+    @message = exception.message
+    @backtrace = exception.backtrace.join("\n") unless exception.nil?
+    logger.info @message
+    logger.info @backtrace
+    render :file => "#{RAILS_ROOT}/app/views/errors/404.rhtml", :layout=> false, :status => 404
+  end #if RAILS_ENV == 'production'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+def default_user
+  params = {}
+  params[:password] = "jphiri"
+  params[:first_name] = "James"
+  params[:last_name] = "Phiri"
+  params[:email] = "jamesphiri@gmail.com"
+  params[:phone_number] = "099900001"
+  params[:username] = "jphiri"
+  params[:role] = "Admin"
+  user = User.new_user(params).save
+  UserRole.create_user_role(user, params)
+end
+
+default_user
